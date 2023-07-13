5b3634ced06f9d08eae52c39307fd8d966740876
sahara.ap <sahara.ap13@gmail.com>
2023-07-13 17:43:06 +0300

    Добавили сообщение об ошибке, если не удалось получить данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 // Модуль - точка входа
 import {renderGallery} from './galleryRender.js';
+import renderStatus from './status.js';
 import './upload.js';
 import { request } from './utils.js';
 
-/**
- * @type {Array<Picture>}
- */
-const data = await request('https://29.javascript.pages.academy/kekstagram/data');
+try {
+  /**
+   * @type {Array<Picture>}
+   */
+  const data = await request('https://29.javascript.pages.academy/kekstagram/data.');
 
-renderGallery(data);
+  renderGallery(data);
+
+} catch (error) {
+  const title = `Ошибка: ${error.message}`;
+  const button = 'Закрыть';
+
+  renderStatus('error', {title, button});
+}
